fa78974f5d405fbe533eb479c81effe87f969237
Carlos <carlos@ai-dev-team.local>
2026-03-12 00:52:02 -0300

    refactor: consolidate duplicate icons in NotificationItem component
    - Extract reusable SVG icons (taskIcon, sprintIcon, commentIcon, etc.)
    - task_assigned and task_unassigned now use the same icon definition
    - sprint_started and sprint_completed now use the same icon definition
    - Reduces code duplication and improves maintainability
    - Easier to update icons globally if needed in the future

--- src/components/NotificationCenter/NotificationItem.tsx
+++ src/components/NotificationCenter/NotificationItem.tsx
@@ -26,78 +26,74 @@ export function NotificationItem({
   onDismiss,
   isDismissing,
 }: NotificationItemProps) {
+  // Reusable SVG icon components
+  const taskIcon = (
+    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+      <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
+      <path
+        fillRule="evenodd"
+        d="M4 5a2 2 0 012-2 1 1 0 000-2 4 4 0 00-4 4v10a4 4 0 004 4h12a4 4 0 004-4V5a4 4 0 00-4-4 1 1 0 000 2 2 2 0 012 2v10a2 2 0 01-2 2H6a2 2 0 01-2-2V5z"
+        clipRule="evenodd"
+      />
+    </svg>
+  )
+
+  const sprintIcon = (
+    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+      <path
+        fillRule="evenodd"
+        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v2a1 1 0 001 1h1v3H4a2 2 0 00-2 2v2a1 1 0 001 1h1v1a1 1 0 102 0v-1h6v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1v-3h1a1 1 0 100-2h-1V7h1a1 1 0 100-2h-1V4a2 2 0 00-2-2h-1V3a1 1 0 00-1-1zm2 5a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1z"
+        clipRule="evenodd"
+      />
+    </svg>
+  )
+
+  const commentIcon = (
+    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+      <path
+        fillRule="evenodd"
+        d="M2 5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm3.707 1.293a1 1 0 00-1.414 1.414L5.586 10l-1.293 1.293a1 1 0 101.414 1.414L7 11.414l1.293 1.293a1 1 0 001.414-1.414L8.414 10l1.293-1.293a1 1 0 00-1.414-1.414L7 8.586 5.707 7.293z"
+        clipRule="evenodd"
+      />
+    </svg>
+  )
+
+  const statusIcon = (
+    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+      <path
+        fillRule="evenodd"
+        d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 1111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 105.199 7.99V4a1 1 0 01-1-1V2a1 1 0 011-1zm7 5a1 1 0 011 1v5a1 1 0 11-2 0V8a1 1 0 011-1z"
+        clipRule="evenodd"
+      />
+    </svg>
+  )
+
+  const agentIcon = (
+    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+      <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+    </svg>
+  )
+
+  const alertIcon = (
+    <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+      <path
+        fillRule="evenodd"
+        d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+        clipRule="evenodd"
+      />
+    </svg>
+  )
+
   // SVG icon components for each notification type
   const iconMap: Record<NotificationType, React.ReactNode> = {
-    task_assigned: (
-      <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
-        <path
-          fillRule="evenodd"
-          d="M4 5a2 2 0 012-2 1 1 0 000-2 4 4 0 00-4 4v10a4 4 0 004 4h12a4 4 0 004-4V5a4 4 0 00-4-4 1 1 0 000 2 2 2 0 012 2v10a2 2 0 01-2 2H6a2 2 0 01-2-2V5z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-    task_unassigned: (
-      <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
-        <path
-          fillRule="evenodd"
-          d="M4 5a2 2 0 012-2 1 1 0 000-2 4 4 0 00-4 4v10a4 4 0 004 4h12a4 4 0 004-4V5a4 4 0 00-4-4 1 1 0 000 2 2 2 0 012 2v10a2 2 0 01-2 2H6a2 2 0 01-2-2V5z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-    sprint_started: (
-      <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-        <path
-          fillRule="evenodd"
-          d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v2a1 1 0 001 1h1v3H4a2 2 0 00-2 2v2a1 1 0 001 1h1v1a1 1 0 102 0v-1h6v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1v-3h1a1 1 0 100-2h-1V7h1a1 1 0 100-2h-1V4a2 2 0 00-2-2h-1V3a1 1 0 00-1-1zm2 5a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-    sprint_completed: (
-      <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-        <path
-          fillRule="evenodd"
-          d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v2a1 1 0 001 1h1v3H4a2 2 0 00-2 2v2a1 1 0 001 1h1v1a1 1 0 102 0v-1h6v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1v-3h1a1 1 0 100-2h-1V7h1a1 1 0 100-2h-1V4a2 2 0 00-2-2h-1V3a1 1 0 00-1-1zm2 5a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-    comment_added: (
-      <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-        <path
-          fillRule="evenodd"
-          d="M2 5a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm3.707 1.293a1 1 0 00-1.414 1.414L5.586 10l-1.293 1.293a1 1 0 101.414 1.414L7 11.414l1.293 1.293a1 1 0 001.414-1.414L8.414 10l1.293-1.293a1 1 0 00-1.414-1.414L7 8.586 5.707 7.293z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-    status_changed: (
-      <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-        <path
-          fillRule="evenodd"
-          d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 1111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 105.199 7.99V4a1 1 0 01-1-1V2a1 1 0 011-1zm7 5a1 1 0 011 1v5a1 1 0 11-2 0V8a1 1 0 011-1z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-    agent_event: (
-      <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-        <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
-      </svg>
-    ),
-    performance_alert: (
-      <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
+    task_assigned: taskIcon,
+    task_unassigned: taskIcon,
+    sprint_started: sprintIcon,
+    sprint_completed: sprintIcon,
+    comment_added: commentIcon,
+    status_changed: statusIcon,
+    agent_event: agentIcon,
+    performance_alert: alertIcon,
   }
 
   // Label map for each notification type
