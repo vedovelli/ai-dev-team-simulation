1944ea3a212f20096279142f2d6a15647531e754
Ana Silva <ana@aidev.team>
2026-03-18 00:13:35 -0300

    chore: export NotificationPreferencesPanel from components
    - Makes NotificationPreferencesPanel easily importable from main components index
    - Enables broader usage of the panel throughout the application

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 // Filter and List Components
 export { TaskFilterPanel } from './TaskFilterPanel/TaskFilterPanel'
 export { InfiniteTaskList } from './InfiniteTaskList/InfiniteTaskList'
+
+// Notification Preferences
+export { NotificationPreferencesPanel } from './NotificationPreferencesPanel/NotificationPreferencesPanel'
